8c3a212e2be30b3192d470c85eafd37df4724e1e
Falk-D <falk.deppe@gmail.com>
2015-05-17 20:36:46 +0200

    Moved voting logic, and applied some of svens suggested changes

--- app/src/main/java/org/milderjoghurt/rlf/android/VoteFragment.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/VoteFragment.java
@@ -4,19 +4,31 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log; // for demonstration/testing
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link VoteFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
+ * Fragment for voting functionality
  */
 public class VoteFragment extends Fragment {
 
+    // for logging
+    private static final String LOG_TAG = "Voting";
+
+    // vote symbols
+    public static final int[] VOTES = { 0, 1, 2, 3};
+
+    // determine which button maps to which vote symbol
+    private Button[] voteMapping = { null, null, null, null };
+
+    // last vote or -1 if not available
+    private int lastVoteSymbol = -1;
+
+    // auto-generated
     private OnFragmentInteractionListener mListener;
 
     public VoteFragment() {
@@ -32,6 +44,7 @@ public class VoteFragment extends Fragment {
     }
 
     // TODO: Rename method, update argument and hook method into UI event
+    // FIXME auto-generated
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -47,6 +60,45 @@ public class VoteFragment extends Fragment {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+
+        // fill vote map
+        voteMapping[0] = (Button) getView().findViewById(R.id.btnVoteA);
+        voteMapping[1] = (Button) getView().findViewById(R.id.btnVoteB);
+        voteMapping[2] = (Button) getView().findViewById(R.id.btnVoteC);
+        voteMapping[3] = (Button) getView().findViewById(R.id.btnVoteD);
+    }
+
+    /**
+     * Should be called when user clicks on a vote button
+     */
+    public void onVoteClick(View src) {
+
+        Button btnSource = (Button) src;
+        if(btnSource == null)
+            return;
+
+        // search for possible vote source to map to a symbol
+        for(int i=0;i<voteMapping.length;++i) {
+            if(voteMapping[i] == btnSource)
+                setVoteSymbol(VOTES[i]);
+        }
+
+        Log.d(LOG_TAG, "vote " + getLastVoteSymbol() + " selected");
+    }
+
+    /**
+     * Should be called when vote was chosen by user and is about to be sended to host
+     */
+    public void onVoteSend(View src) {
+        Log.d(LOG_TAG, "vote is queried for sending");
+    }
+
+    private void setVoteSymbol(int i) {
+        lastVoteSymbol = i;
+    }
+
+    public int getLastVoteSymbol() {
+        return lastVoteSymbol;
     }
 
     @Override
@@ -64,9 +116,11 @@ public class VoteFragment extends Fragment {
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
+     * FIXME auto-generated
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
+        // --> auto-generated
         public void onFragmentInteraction(Uri uri);
     }
 
