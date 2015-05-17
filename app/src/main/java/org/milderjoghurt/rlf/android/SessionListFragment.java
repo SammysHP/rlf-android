bfe49feb6dea45f31dd5fbd34887e0dd07bec578
Fjen Undso <fjen@neboola.de>
2015-05-18 00:53:13 +0200

    add initial session list with dummy data

---
+++ app/src/main/java/org/milderjoghurt/rlf/android/SessionListFragment.java
@@ -0,0 +1,45 @@
+package org.milderjoghurt.rlf.android;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.milderjoghurt.rlf.android.dummy.DummyAdapter;
+
+
+public class SessionListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_session_list, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.session_list);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter
+        mAdapter = new DummyAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/org/milderjoghurt/rlf/android/dummy/DummyAdapter.java
@@ -0,0 +1,62 @@
+package org.milderjoghurt.rlf.android.dummy;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.milderjoghurt.rlf.android.R;
+
+/**
+ * Dummy Content Adapter for Session List
+ */
+public class DummyAdapter extends RecyclerView.Adapter<DummyAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public View mView;
+        public ViewHolder(View v) {
+            super(v);
+            mView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public DummyAdapter() {
+        mDataset = new String[3];
+        mDataset[0] = "foo";
+        mDataset[1] = "bar";
+        mDataset[2] = "baz";
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public DummyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_session_entry, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        ((TextView) holder.mView.findViewById(R.id.textView)).setText(mDataset[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
