371867b4113873f332bc16f228c9a42a5cb96cf2
Fjen Undso <fjen@neboola.de>
2015-05-18 20:11:21 +0200

    add floating button to sessionlist

--- app/src/main/java/org/milderjoghurt/rlf/android/SessionListActivity.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/SessionListActivity.java
@@ -1,11 +1,11 @@
 package org.milderjoghurt.rlf.android;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import java.util.Random;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 public class SessionListActivity extends AppCompatActivity {
 
@@ -13,5 +13,14 @@ public class SessionListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_list);
+
+        final FloatingActionButton addSession = (FloatingActionButton) findViewById(R.id.session_list_fab);
+        addSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rnd = new Random();
+                addSession.setColorNormal(rnd.nextInt());
+            }
+        });
     }
 }
