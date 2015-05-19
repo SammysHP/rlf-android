ef1f7bb1cf97ad4883ec619ad1adb6151676f94c
SammysHP <sven@sammyshp.de>
2015-05-19 11:31:49 +0200

    Revert "StartPageFragment and Activity und Layout erzeugt."
    This reverts commit 9a5f988c1eb7eebd025deee64776c954d15d52a2.

--- app/src/main/java/org/milderjoghurt/rlf/android/StartPage.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/StartPage.java
@@ -13,12 +13,12 @@ import android.widget.RadioButton;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link StartPageFragment.OnFragmentInteractionListener} interface
+ * {@link StartPage.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link StartPageFragment#newInstance} factory method to
+ * Use the {@link StartPage#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StartPageFragment extends Fragment {
+public class StartPage extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -36,11 +36,11 @@ public class StartPageFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment StartPageFragment.
+     * @return A new instance of fragment StartPage.
      */
     // TODO: Rename and change types and number of parameters
-    public static StartPageFragment newInstance(String param1, String param2) {
-        StartPageFragment fragment = new StartPageFragment();
+    public static StartPage newInstance(String param1, String param2) {
+        StartPage fragment = new StartPage();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -48,7 +48,7 @@ public class StartPageFragment extends Fragment {
         return fragment;
     }
 
-    public StartPageFragment() {
+    public StartPage() {
         // Required empty public constructor
     }
 
