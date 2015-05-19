39599ebf1582bd28f9ddde6288097df1490bfb99
Falk <falk.deppe@gmail>
2015-05-19 10:42:15 +0200

    Fixed crashing of vote activity (refactored fragment setup and used different lib for FragmentActivity)

--- app/src/main/java/org/milderjoghurt/rlf/android/VoteActivity.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/VoteActivity.java
@@ -1,23 +1,16 @@
 package org.milderjoghurt.rlf.android;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.support.v4.app.FragmentActivity;
 
 /**
- * TODO app crashes when debugging with this activity ...
+ *
  */
-public class VoteActivity extends AppCompatActivity {
+public class VoteActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
-
 }
