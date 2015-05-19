6ff9238314b1b3f4b1096dfd9389ebad2e010b88
Sebastian Foellmer <sebastian.foellmer@gmail.com>
2015-05-19 19:12:50 +0200

    StundentLive Aktivity mit Fragment

---
+++ app/src/main/java/org/milderjoghurt/rlf/android/StudentLiveActivity.java
@@ -0,0 +1,20 @@
+package org.milderjoghurt.rlf.android;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class StudentLiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_live);
+    }
+
+
+
+}
---
+++ app/src/main/java/org/milderjoghurt/rlf/android/StudentLiveFragment.java
@@ -0,0 +1,18 @@
+package org.milderjoghurt.rlf.android;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StudentLiveFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_student_live, container, false);
+
+
+    }
+}
