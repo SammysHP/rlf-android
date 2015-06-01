8be87bc494be2d90d54c27a2832ee25de776cbbb
SammysHP <sven@sammyshp.de>
2015-06-01 14:16:31 +0200

    Cleanup and improve StartActivity
    - Cleanup menu
    - Tried little eye candy for EditText
    - Limit EditText to 8 chars
    - Automatically open soft keyboard
    - Set width of EditText to 8 chars
    - Join session with keyboard key

--- app/src/main/java/org/milderjoghurt/rlf/android/StartPageFragment.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/StartPageFragment.java
@@ -3,14 +3,17 @@ package org.milderjoghurt.rlf.android;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StartPageFragment extends Fragment {
@@ -26,20 +29,28 @@ public class StartPageFragment extends Fragment {
         btnEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getView().getContext(), StudentLiveActivity.class);
+                enterSession();
+            }
+        });
 
-                EditText editText = (EditText) getView().findViewById(R.id.SessionID);
-                if (editText.getText().toString().length() != 6) {
-                    Toast.makeText(getActivity(), "Falsche ID", Toast.LENGTH_SHORT).show();
-                } else {
-                    intent.putExtra("Titel", editText.getText().toString());
-                    startActivity(intent);
-                }
+        EditText idInput = (EditText) view.findViewById(R.id.SessionID);
+        idInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                enterSession();
+                return true;
             }
         });
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getView().findViewById(R.id.SessionID).requestFocus();
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -56,6 +67,18 @@ public class StartPageFragment extends Fragment {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void enterSession() {
+        Intent intent = new Intent(getView().getContext(), StudentLiveActivity.class);
+
+        EditText editText = (EditText) getView().findViewById(R.id.SessionID);
+        if (editText.getText().toString().length() != 6) {
+            Toast.makeText(getActivity(), "Falsche ID", Toast.LENGTH_SHORT).show();
+        } else {
+            intent.putExtra("Titel", editText.getText().toString());
+            startActivity(intent);
         }
     }
 }
