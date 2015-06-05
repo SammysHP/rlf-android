a81b64086b5982ad5808257d7a187952e4af13cc
Ferdinand <ferdinand.roever@t-online.de>
2015-06-05 13:41:05 +0200

    Startactivity wurde verändert. Bei Eingabe von 0-9 blinkt das Eingabefeld grün auf. Bei anderen Eingaben sollte es Rot aufblinken. Das funktioniert noch nicht.

--- app/src/main/java/org/milderjoghurt/rlf/android/StartPageActivity.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/StartPageActivity.java
@@ -1,7 +1,14 @@
 package org.milderjoghurt.rlf.android;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.EditText;
+
+
 
 public class StartPageActivity extends AppCompatActivity {
     @Override
@@ -9,4 +16,84 @@ public class StartPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startpage);
     }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_1:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_2:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_3:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_4:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_5:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_6:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_7:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_8:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_9:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_0:
+                blinkGreen();
+                return true;
+            case KeyEvent.KEYCODE_NUMPAD_COMMA:
+                blinkRed();
+                return true;
+            default:
+                blinkRed();
+                return true;
+        }
+    }
+
+
+
+
+    public void blinkGreen() {
+        green();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                white();
+            }
+        }, 500);
+    }
+
+    public void blinkRed() {
+        red();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                white();
+            }
+        }, 500);
+    }
+
+
+    public void green(){
+        ((EditText) findViewById(R.id.SessionID)).getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_ATOP);
+    }
+
+    public void white(){
+        ((EditText) findViewById(R.id.SessionID)).getBackground().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+    }
+
+    public void red(){
+        ((EditText) findViewById(R.id.SessionID)).getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+    }
+
+
 }
