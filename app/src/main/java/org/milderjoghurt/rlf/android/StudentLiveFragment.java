3a38f2d6aa78957834d9cbebd8d65f775fe2b2a5
Sebastian Foellmer <sebastian.foellmer@gmail.com>
2015-06-14 21:14:05 +0200

    StudentLive eigefärbter "Melden" Button

--- app/src/main/java/org/milderjoghurt/rlf/android/StudentLiveFragment.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/StudentLiveFragment.java
@@ -1,6 +1,8 @@
 package org.milderjoghurt.rlf.android;
 
 import android.app.Fragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,10 @@ import android.widget.Button;
 
 
 public class StudentLiveFragment extends Fragment {
+
+    private boolean isPressed = false;
+    private Button signal_btn;
+    private static final int unselectedColor = R.color.button_material_light;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,9 +28,23 @@ public class StudentLiveFragment extends Fragment {
         super.onActivityCreated(savedInstance);
 
      //   Button break1_btn = (Button) getView().findViewById(R.id.break1);
-        Button signal_btn = (Button) getView().findViewById(R.id.signal);
+        signal_btn = (Button) getView().findViewById(R.id.signal);
+        signal_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPressed = !isPressed;
 
-
+                if (isPressed) {
+                    signal_btn.setBackgroundColor(Color.parseColor("#FF9900"));
+                } else {
+                    signal_btn.setBackgroundColor(Color.LTGRAY);
+                    //signal_btn.setBackgroundResource(android.R.drawable.btn_default);
+                }
+            }
+        });
+        //return view;
     }
+
+
 
 }
