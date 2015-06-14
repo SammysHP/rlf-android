5d0e36f5f7a7b02e24f0827253ce64817cfff8be
Ferdinand <ferdinand.roever@t-online.de>
2015-06-14 18:11:20 +0200

    StartpageActivity wurde verändert. Textfarbe ist grau solange nicht 6 Ziffern eingegeben wurden.

--- app/src/main/java/org/milderjoghurt/rlf/android/StartPageActivity.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/StartPageActivity.java
@@ -1,12 +1,27 @@
 package org.milderjoghurt.rlf.android;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.EditText;
 
 public class StartPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startpage);
+    }
+
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+        if (((EditText) findViewById(R.id.SessionID)).getText().toString().length() != 6) {
+            ((EditText) findViewById(R.id.SessionID)).setHintTextColor(Color.GRAY);
+            ((EditText) findViewById(R.id.SessionID)).setTextColor(Color.GRAY);
+        } else {
+            ((EditText) findViewById(R.id.SessionID)).setHintTextColor(Color.BLACK);
+            ((EditText) findViewById(R.id.SessionID)).setTextColor(Color.BLACK);
+        }
+        return true;
     }
 }
