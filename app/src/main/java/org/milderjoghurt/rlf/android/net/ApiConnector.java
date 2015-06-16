272397c5ee55c3700228dc34520e47354af4b445
SammysHP <sven@sammyshp.de>
2015-06-16 17:36:35 +0200

    Add ApiConnector
    Response deserialization is not part of this class and happens in the handler
    (instance of AsyncHttpResponseHandler).

---
+++ app/src/main/java/org/milderjoghurt/rlf/android/net/ApiConnector.java
@@ -0,0 +1,114 @@
+package org.milderjoghurt.rlf.android.net;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.milderjoghurt.rlf.android.dummy.Session;
+
+public class ApiConnector {
+    private static final class Constants {
+        static final String BASE_URL = "http://krul.finf.uni-hannover.de/todo/"; // TODO
+        static final String SESSIONS = "sessions";
+        static final String SESSION = "sessions/";
+        static final String SESSIONS_FROM = "sessions/from/";
+        static final String VOTES = "/votes";
+        static final String ANSWERS = "/answers";
+        static final String RESET_ANSWERS = "/resetanswers/";
+        static final String VOTE = "votes";
+        static final String ANSWER = "answers";
+
+        private Constants() {
+            // Do not instantiate
+        }
+    }
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    private static void get(final String url, final RequestParams params, final AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static void post(final String url, final RequestParams params, final AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static void put(final String url, final RequestParams params, final AsyncHttpResponseHandler responseHandler) {
+        client.put(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static void delete(final String url, final AsyncHttpResponseHandler responseHandler) {
+        client.delete(getAbsoluteUrl(url), responseHandler);
+    }
+
+    private static String getAbsoluteUrl(final String relativeUrl) {
+        return Constants.BASE_URL + relativeUrl;
+    }
+
+    public void getSessions(final AsyncHttpResponseHandler handler) {
+        get(Constants.SESSIONS, null, handler);
+    }
+
+    public void getSessionsByOwner(final String owner, final AsyncHttpResponseHandler handler) {
+        get(Constants.SESSIONS_FROM + owner, null, handler);
+    }
+
+    public void getSession(final String sessionId, final AsyncHttpResponseHandler handler) {
+        get(Constants.SESSION + sessionId, null, handler);
+    }
+
+    public void createSession(final Session session, final AsyncHttpResponseHandler handler) {
+        // TODO: transform session into POST params
+        final RequestParams params = new RequestParams();
+        params.put("key", "value");
+        params.put("more", "data");
+
+        post(Constants.SESSIONS, params, handler);
+    }
+
+    public void updateSession(final Session session, final AsyncHttpResponseHandler handler) {
+        // TODO: get ID from session object
+        final String sessionId = "todo";
+
+        // TODO: transform session into POST params
+        final RequestParams params = new RequestParams();
+        params.put("key", "value");
+        params.put("more", "data");
+
+        put(Constants.SESSION + sessionId, params, handler);
+    }
+
+    public void deleteSession(final String sessionId, final String owner, final AsyncHttpResponseHandler handler) {
+        delete(Constants.SESSION + sessionId + '/' + owner, handler);
+    }
+
+    public void getVotes(final String sessionId, final AsyncHttpResponseHandler handler) {
+        get(Constants.SESSION + sessionId + Constants.VOTES, null, handler);
+    }
+
+    public void getAnswers(final String sessionId, final AsyncHttpResponseHandler handler) {
+        get(Constants.SESSION + sessionId + Constants.ANSWERS, null, handler);
+    }
+
+    public void resetAnswers(final String sessionId, final String owner, final AsyncHttpResponseHandler handler) {
+        get(Constants.SESSION + sessionId + Constants.RESET_ANSWERS + owner, null, handler);
+    }
+
+    public void createAnswer(final String sessionId, final AsyncHttpResponseHandler handler) {
+        // TODO: transform (new parameter!) answer to POST params
+        final RequestParams params = new RequestParams();
+        params.put("key", "value");
+        params.put("more", "data");
+
+        post(Constants.ANSWER + sessionId, null, handler);
+    }
+
+    public void createVote(final String sessionId, final AsyncHttpResponseHandler handler) {
+        // TODO: transform (new parameter!) answer to POST params
+        final RequestParams params = new RequestParams();
+        params.put("key", "value");
+        params.put("more", "data");
+
+        post(Constants.VOTE + sessionId, null, handler);
+    }
+}
