95b34f30a57c3cbc5d2fc569df2be5125045f1f0
SammysHP <sven@sammyshp.de>
2015-06-21 19:21:37 +0200

    Make ApiResponseHandler methods abstract

--- app/src/main/java/org/milderjoghurt/rlf/android/net/ApiResponseHandler.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/net/ApiResponseHandler.java
@@ -3,9 +3,7 @@ package org.milderjoghurt.rlf.android.net;
 import org.milderjoghurt.rlf.android.models.Model;
 
 public abstract class ApiResponseHandler<M extends Model> {
-    public void onSuccess(final M model) {
-    }
+    public abstract void onSuccess(final M model);
 
-    public void onFailure(Throwable e) {
-    }
+    public abstract void onFailure(Throwable e);
 }
