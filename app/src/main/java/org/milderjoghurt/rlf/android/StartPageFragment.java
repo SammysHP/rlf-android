0bbe1bf8c812d34c842e1259451639b2c49ff1df
Ferdinand <ferdinand.roever@t-online.de>
2015-06-23 15:01:59 +0200

    StartPageActivity: Button ist ausgegraut, solange nicht 6 Zeichen vorhanden sind.

--- app/src/main/java/org/milderjoghurt/rlf/android/StartPageFragment.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/StartPageFragment.java
@@ -1,16 +1,14 @@
 package org.milderjoghurt.rlf.android;
-
 import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -18,16 +16,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 public class StartPageFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_start_page, container, false);
+        final View view = inflater.inflate(R.layout.fragment_start_page, container, false);
         setHasOptionsMenu(true);
 
+        EditText editText = (EditText) view.findViewById(R.id.SessionID);
+
+        editText.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if (((EditText) getView().findViewById(R.id.SessionID)).
+                        getText().toString().length() == 6){
+                    ((Button) view.findViewById(R.id.btnEnterSession)).setEnabled(true);
+                }else{
+                    ((Button) view.findViewById(R.id.btnEnterSession)).setEnabled(false);
+                }
+            }
+        });
         Button btnEnter = (Button) view.findViewById(R.id.btnEnterSession);
         btnEnter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +51,6 @@ public class StartPageFragment extends Fragment {
                 enterSession();
             }
         });
-
         Button btnListSessions = (Button) view.findViewById(R.id.btnListSessions);
         btnListSessions.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,8 +59,7 @@ public class StartPageFragment extends Fragment {
             }
         });
 
-        EditText idInput = (EditText) view.findViewById(R.id.SessionID);
-        idInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 enterSession();
@@ -54,20 +68,15 @@ public class StartPageFragment extends Fragment {
         });
         return view;
     }
-
     @Override
     public void onResume() {
         super.onResume();
-
         getView().findViewById(R.id.SessionID).requestFocus();
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
-
     private void enterSession() {
         Intent intent = new Intent(getView().getContext(), StudentLiveActivity.class);
-
         final EditText editText = (EditText) getView().findViewById(R.id.SessionID);
-
         if (editText.getText().toString().length() != 6) {
             Toast.makeText(getActivity(), "zu kurze ID", Toast.LENGTH_SHORT).show();
             editText.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
@@ -78,7 +87,7 @@ public class StartPageFragment extends Fragment {
                 }
             }, 500);
         } else {
-            intent.putExtra(StudentLiveActivity.EXTRA_ID, editText.getText().toString());
+            intent.putExtra("Titel", editText.getText().toString());
             startActivity(intent);
         }
     }
