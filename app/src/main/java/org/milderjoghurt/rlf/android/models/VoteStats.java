c1694d042463474332a192bfcd1db894469d7752
Fjen Undso <fjen@neboola.de>
2015-06-23 14:01:07 +0200

    models: add VoteStats and update Vote

---
+++ app/src/main/java/org/milderjoghurt/rlf/android/models/VoteStats.java
@@ -0,0 +1,32 @@
+package org.milderjoghurt.rlf.android.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Date;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class VoteStats {
+    public Long id;
+
+    public Date date = new Date();
+
+    public Integer value;
+
+    public Type type;
+
+    public enum Type {
+        ALL,
+        SPEED,
+        UNDERSTANDABILITY,
+        REQUEST,
+    }
+
+    public VoteStats() {
+    }
+
+    public VoteStats(Type type, Integer value) {
+        this.type = type;
+        this.value = value;
+    }
+
+}
