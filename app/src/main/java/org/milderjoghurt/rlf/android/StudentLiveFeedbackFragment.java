6118d53939d03fc23dfe5c4d752224869172cacc
Sebastian Foellmer <sebastian.foellmer@gmail.com>
2015-06-25 14:31:26 +0200

    Redesign Student Live Activity

--- app/src/main/java/org/milderjoghurt/rlf/android/StudentLiveFeedbackFragment.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/StudentLiveFeedbackFragment.java
@@ -6,13 +6,24 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 
 import org.milderjoghurt.rlf.android.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
+
+
+
+
 public class StudentLiveFeedbackFragment extends Fragment {
+
+    private boolean isPressed = false;
+    private Button signal_btn;
+    private static final int unselectedColor = R.color.button_material_light;
 
 
     public StudentLiveFeedbackFragment() {
@@ -27,5 +38,29 @@ public class StudentLiveFeedbackFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_student_live_feedback, container, false);
     }
 
+
+
+    public void onActivityCreated(Bundle savedInstance) {
+    super.onActivityCreated(savedInstance);
+
+    //   Button break1_btn = (Button) getView().findViewById(R.id.break1);
+    signal_btn = (Button) getView().findViewById(R.id.signal);
+    signal_btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            isPressed = !isPressed;
+
+            if (isPressed) {
+                signal_btn.setBackgroundColor(Color.parseColor("#FF9900"));
+                signal_btn.setText("Hand senken");
+            } else {
+                signal_btn.setBackgroundColor(Color.LTGRAY);
+                signal_btn.setText("Hand heben");
+                //signal_btn.setBackgroundResource(android.R.drawable.btn_default);
+            }
+        }
+    });
+    //return view;
+}
 
 }
