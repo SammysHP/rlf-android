e8bd6de3b0dac7111e4138f3443ad0e6873bdbc4
Nils Hein <nhein@welfenlab.de>
2015-06-28 23:31:01 +0200

    Fixed Issue #47 bei wechsel in Landscape wird das Fragment neu erstellt View aber nicht dadurch Nullpointer

--- app/src/main/java/org/milderjoghurt/rlf/android/ReaderLiveFeedbackFragment.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/ReaderLiveFeedbackFragment.java
@@ -115,9 +115,11 @@ public class ReaderLiveFeedbackFragment extends Fragment {
             public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
                 if (!isChecked) {
                     setFeedbackState(FeedbackState.INACTIVE);
+                }else{
+                    setFeedbackState(FeedbackState.NEUTRAL);
                 }
 
-                if (activeSession.open != isChecked) {
+                if (activeSession != null && activeSession.open != isChecked) {
                     activeSession.open = isChecked;
                     ApiConnector.updateSession(activeSession, ApiConnector.getOwnerId(view.getContext()), new ApiResponseHandler<Session>() {
                         @Override
