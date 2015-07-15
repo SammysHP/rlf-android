1bb33dc3191e62cb3e475c115d70339197ac3ba4
Fjen Undso <fjen@neboola.de>
2015-07-15 14:35:13 +0200

    license project under GPLv3

--- app/src/main/java/org/milderjoghurt/rlf/android/net/ApiResponseHandler.java
+++ app/src/main/java/org/milderjoghurt/rlf/android/net/ApiResponseHandler.java
@@ -1,5 +1,25 @@
-package org.milderjoghurt.rlf.android.net;
+/*
+ * Copyright (C) 2015 MilderJoghurt
+ *
+ * This file is part of Realtime Lecture Feedback for Android.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See COPYING, CONTRIBUTORS for more details.
+ */
 
+package org.milderjoghurt.rlf.android.net;
 
 public abstract class ApiResponseHandler<T> {
     public abstract void onSuccess(final T model);
